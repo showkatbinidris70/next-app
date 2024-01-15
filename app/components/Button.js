66af0a63e0d159ede32cd0eb43d1628d489e21da
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-01-15 18:29:06 +0600

    use client

---
+++ app/components/Button.js
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+
+export default function Button() {
+  return (
+    <div>
+      <button
+        className="bg-slate-300 text-blue-900 mt-3 px-3 py-2 rounded-sm"
+        onClick={() => console.log("clicked")}
+      >
+        Click Here
+      </button>
+    </div>
+  );
+}
